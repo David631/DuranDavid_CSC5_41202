c546eaac57b9286f6d32624c6fb30b3d471d623c
David631 <dav600d@yahoo.com>
2016-01-05 11:47:55 -0800

    Signed-off-by: David631 <dav600d@yahoo.com>

---
+++ Class/NoMistake/main.cpp
@@ -0,0 +1,34 @@
+/* 
+    Author: David Duran
+    Created on January 5, 2016, 11:12 AM
+    Purpose: No Mistakes
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+// Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short c,b=20000,a=30000;
+        
+    //Calculate or map inputs to outputs
+    c=a+b;
+    
+    //Output the results
+    cout<<   "a = " <<a<<endl;
+    cout<<   "b = " <<b<<endl;
+    cout<<c<<" = "  <<a<<"+"<<b<<endl;
+       
+    
+    //Exit stage right
+    return 0;
+}
+
