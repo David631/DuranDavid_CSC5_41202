8c33f16ab4d1da4e3a3a8c863e61b644bf533c31
David631 <dav600d@yahoo.com>
2016-01-11 19:50:20 -0800

    Signed-off-by: David631 <dav600d@yahoo.com>

--- Lab Section/Truth_Table/nbproject/Makefile-variables.mk
+++ Lab Section/Truth_Table/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=q1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/q1
+CND_ARTIFACT_NAME_Debug=truth_table
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/truth_table
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=q1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/q1.tar
+CND_PACKAGE_NAME_Debug=truthtable.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/truthtable.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=q1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/q1
+CND_ARTIFACT_NAME_Release=truth_table
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/truth_table
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=q1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/q1.tar
+CND_PACKAGE_NAME_Release=truthtable.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/truthtable.tar
 #
 # include compiler specific variables
 #
