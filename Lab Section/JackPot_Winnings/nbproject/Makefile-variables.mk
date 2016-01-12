7c3c75e7884b292b159c6770b560b567fdf399a1
David631 <dav600d@yahoo.com>
2016-01-11 18:55:39 -0800

    Signed-off-by: David631 <dav600d@yahoo.com>

--- Lab Section/JackPot_Winnings/nbproject/Makefile-variables.mk
+++ Lab Section/JackPot_Winnings/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=jackpot_winnings
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/jackpot_winnings
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/jackpot_winnings
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=jackpotwinnings.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/jackpotwinnings.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/jackpotwinnings.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=jackpot_winnings
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/jackpot_winnings
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/jackpot_winnings
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=jackpotwinnings.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/jackpotwinnings.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/jackpotwinnings.tar
 #
 # include compiler specific variables
 #
