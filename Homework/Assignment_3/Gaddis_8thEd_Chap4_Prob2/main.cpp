4cceebcd0b1d753600e34747146c3493ecb8b97d
david631 <dav600d@yahoo.com>
2016-01-20 11:35:16 -0800

    Signed-off-by: david631 <dav600d@yahoo.com>

--- Homework/Assignment_3/Gaddis_8thEd_Chap4_Prob2/main.cpp
+++ Homework/Assignment_3/Gaddis_8thEd_Chap4_Prob2/main.cpp
@@ -1,12 +1,13 @@
 /* 
     File:   main.cpp
     Author: David Duran
-    Created on January 5, 2016, 06:40 AM
-    Purpose: Setup for Home LDE
+    Created on January 20, 2016, 09:52 AM
+    Purpose: Roman Numeral Converter
  */
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -18,15 +19,36 @@ using namespace std;
 // Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
+    int number;//Number between 1-10
     
     //Input data
+    cout<<"Insert a number between 1-10"<<endl;
+    cin>>number;
     
-    //Calculate or map inputs to outputs
-    
-    //Output the results
-    cout<<"Hello World"<<endl;
-    
-    
+    switch(number){
+        case 1:cout<<"I"<<endl;
+                break;
+        case 2:cout<<"II"<<endl;
+                break;
+        case 3:cout<<"III"<<endl;
+                break;
+        case 4:cout<<"IV"<<endl;
+                break;
+        case 5:cout<<"V"<<endl;
+                break;
+        case 6:cout<<"VI"<<endl;
+                break;
+        case 7:cout<<"VII"<<endl;
+                break;
+        case 8:cout<<"VIII"<<endl;
+                break;
+        case 9:cout<<"IX"<<endl;
+                break;
+        case 10:cout<<"X"<<endl;
+                break;
+        default: cout<<"Number Invalid"<<endl;
+    }
+
     //Exit stage right
     return 0;
 }
---
+++ Homework/Assignment_4/Gaddis_8thEd_Chap5_Prob7/main.cpp
@@ -0,0 +1,42 @@
+/* 
+    Version: 1
+    Author: David Duran
+    Created on January 20, 2016, 11:13 AM
+    Purpose: Table of Salary Doubling starting at a penny
+ */
+
+//System Libraries
+#include <iostream> //I/O
+#include <iomanip>  //Formatting
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+// Execution Begins Here
+int main(int argc, char** argv) {
+    //Problem to solve
+    cout<<endl<<"Solution to Gaddis 8thEd Chap5 Prob7"<<endl;
+    cout<<endl<<"Display a Salary Table"<<endl<<endl;
+   
+    //Declare Variables
+    unsigned int salary=1;      //Salary starting at a penny
+    float totlPay=salary;         //Total Pay by summing the salary
+    
+    //Loop to generate the Salary Table and Total Paid
+    cout<<" Day        $Salary         $Total"<<endl;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<setw(4)<<1<<setw(15)<<salary/100.0f<<setw(15)<<totlPay/100.0f<<endl;
+    for(int day=2;day<=31;day++){
+        //salary*=2;
+        salary<<=1;//Bit Shift right by 1 bit
+        totlPay+=salary;
+        cout<<setw(4)<<day<<setw(15)<<salary/100.0<<setw(15)<<totlPay/100.0<<endl;
+    }
+    
+    //Exit stage right
+    return 0;
+}
