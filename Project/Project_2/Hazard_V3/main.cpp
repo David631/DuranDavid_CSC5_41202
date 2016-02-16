f8a0e49c91ec18ff2e1fbe9a550f2a9dd739e297
david631 <dav600d@yahoo.com>
2016-02-15 23:32:19 -0800

    Signed-off-by: david631 <dav600d@yahoo.com>

--- Project/Project_2/Hazard_V3/main.cpp
+++ Project/Project_2/Hazard_V3/main.cpp
@@ -19,11 +19,10 @@ using namespace std;
 
 //Function Prototypes
 void dsplFil(unsigned short&,unsigned short&,unsigned short&,float&,float&);
+void dsplInt();
 void fillAry(int [],int);
 void prntAry(int [],int,int);
-void swap(int &,int &);
-void lstSmal(int [],int,int);
-void markSrt(int [],int);
+void bublSrt(int [],int);
 // Execution Begins Here
 int main(int argc, char** argv) {
     //Set the random number seed
@@ -33,13 +32,18 @@ int main(int argc, char** argv) {
     float bet,wallet;             //Amount $ able to bet and Amount on user
     unsigned int sum,sum2,main;   //Main = #5-9, sum=die1+die2, sum2=die1+die2
     unsigned short wins=0,losses=0,count=0;//Number of Wins, Losses, and Games
-    char retry='y',die1,die2;     //Dice #1: 1-6,Dice #2: 1=6,Replay
-    const int SIZE=10;
+    char retry='y',die1,die2,instrut;     //Dice #1: 1-6,Dice #2: 1=6,Replay
+    const int SIZE=100;
     int array[SIZE];
-    ofstream out;                  //Allows the File to be read
+    ofstream out,info;                  //Allows the File to be read
     
     //Hazard Game Setup
     cout<<"Welcome to The Game of 'Hazard'!"<<endl;
+    cout<<"Would you like to read the Rules?(y/n)"<<endl;
+    cin>>instrut;
+    if(instrut=='y'){
+        dsplInt();
+    }
     cout<<"How Much $$ are you carrying?"<<endl;
     cin>>wallet;
     cout<<"How much $$ would you like to Bet/Game?(Limit=$1500)"<<endl;
@@ -157,6 +161,7 @@ int main(int argc, char** argv) {
     cout<<"Percentage of Games Lost: "<<100.0f*losses/count<<"%"<<endl;
     cout<<"Current Wallet: $"<<wallet<<endl;
     cout<<"Bet/Game: "<<bet<<endl;
+    
     //Fill the array with random 2 digit numbers
     fillAry(array,SIZE);
     
@@ -164,12 +169,13 @@ int main(int argc, char** argv) {
     prntAry(array,SIZE,10);
     
     //Test finding the smallest number in the list
-    markSrt(array,SIZE);
+    bublSrt(array,SIZE);
             
     //Print the array
     prntAry(array,SIZE,10);
     dsplFil(count,wins,losses,wallet,bet);
     out.close();
+    info.close();
     return 0;
 }
 /******************************************************************************/
@@ -188,10 +194,33 @@ void dsplFil(unsigned short &count,unsigned short &wins,
     out<<"Current Wallet: $"<<wallet<<endl;
     out<<"Bet/Game: "<<bet<<endl;
 }
+/******************************************************************************/
+/*                          Display Instructions                              */
+/******************************************************************************/
+void dsplInt(){
+    ofstream info;
+    info.open("Instructions.dat");
+    info<<"Pick a number between 5 and 9, this is the main."
+           "Then throw two dice.If you roll the main, you win."
+         "If you roll a 2 or a 3, you lose (throws out)."
+          "If you roll an 11 or 12, the result depends on the main:"
+        "with a main of 5 or 9, you throw out with both an 11 and a 12"
+        "with a main of 6 or 8, you throw out with an 11 but nicks with a 12"
+        "with a main of 7, you nick with an 11 but throw out with a 12."
+        "If you neither nick nor throw out, the number thrown is called the chance."
+        "You throw the dice again: if you roll the chance, you win "
+        "if you roll the main, you lose (unlike on the first throw)"
+        "if you roll neither, you keep throwing until you roll"
+        "three times without getting one or the other, winning with the chance,"
+        " losing with the main, and losing without getting either."<<endl;
+}
+/******************************************************************************/
+/*                                      Void 1                                */
+/******************************************************************************/
 void fillAry(int a[],int n){
     //loop and fill the array with random numbers
     for(int i=0;i<n;i++){ 
-        a[i]=rand()%6+1;
+        a[i]=rand()%6+1;//[10,99]
     }
 }
 
@@ -209,30 +238,20 @@ void prntAry(int a[],int n,int perLine){
 }
 
 /******************************************************************************/
-/*                                  Swap Variables                            */
+/*                                 Bubble Sort                                */
 /******************************************************************************/
-void swap(int &a,int &b){
-    a=a^b;
-    b=a^b;
-    a=a^b;
-}
-
-/******************************************************************************/
-/*                                  Smallest in List                           */
-/******************************************************************************/
-void lstSmal(int a[],int n,int pos){
-    //Loop and compare
-    for(int i=pos+1;i<n;i++){
-        if(a[pos]>a[i])swap(a[pos],a[i]);
-    }
-}
-
-/******************************************************************************/
-/*                                                                            */
-/******************************************************************************/
-void markSrt(int a[],int n){
-    //loop and fill the array with random numbers
-    for(int i=0;i<n-1;i++){ 
-        lstSmal(a,n,i);
-    }
+void bublSrt(int a[],int n){
+    //Outside loop to test if any swaps happen
+    bool swap;
+    do{
+        swap=false;
+        for(int j=0;j<n-1;j++){
+            if(a[j]>a[j+1]){
+            int temp=a[j];//Exclusive or XOR
+            a[j]=a[j+1];//Exclusive or XOR
+            a[j+1]=temp;
+            swap=true;
+            }
+        }
+    }while(swap);
 }
